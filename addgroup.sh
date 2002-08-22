f323547eedd142f7eac5f364a319941bab1273ab
TAKAHASHI Kaoru <kaoru@kaisei.org>
2002-08-22 08:18:52 +0000

    Update.

---
+++ addgroup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+/usr/sbin/group add -g 25 smmsp
+/usr/sbin/group add -g 26 popa3d
+/usr/sbin/group add -g 27 sshd
+/usr/sbin/group add -g 28 _portmap
+/usr/sbin/group add -g 29 _identd
+/usr/sbin/group add -g 30 _rstatd
+/usr/sbin/group add -g 32 _rusersd
+/usr/sbin/group add -g 33 _fingerd
+/usr/sbin/group add -g 34 _sshagnt
+/usr/sbin/group add -g 66 crontab
+/usr/sbin/group add -g 71 proxy
+/usr/sbin/group add -g 72 authpf
