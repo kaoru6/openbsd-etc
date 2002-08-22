42e762dd7f07207f5c0a9fee0cef9d6868b9872d
TAKAHASHI Kaoru <kaoru@kaisei.org>
2002-08-22 08:35:24 +0000

    Update.

--- addgroup.sh
+++ addgroup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-/usr/sbin/group add -g 25 smmsp
-/usr/sbin/group add -g 26 popa3d
-/usr/sbin/group add -g 27 sshd
-/usr/sbin/group add -g 28 _portmap
-/usr/sbin/group add -g 29 _identd
-/usr/sbin/group add -g 30 _rstatd
-/usr/sbin/group add -g 32 _rusersd
-/usr/sbin/group add -g 33 _fingerd
-/usr/sbin/group add -g 34 _sshagnt
-/usr/sbin/group add -g 66 crontab
-/usr/sbin/group add -g 71 proxy
-/usr/sbin/group add -g 72 authpf
+group add -g 25 smmsp
+group add -g 26 popa3d
+group add -g 27 sshd
+group add -g 28 _portmap
+group add -g 29 _identd
+group add -g 30 _rstatd
+group add -g 32 _rusersd
+group add -g 33 _fingerd
+group add -g 34 _sshagnt
+group add -g 66 crontab
+group add -g 71 proxy
+group add -g 72 authpf
---
+++ adduser.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+VAREMPTY=' -d /var/empty -s /sbin/nologin '
+NONEXISTENT=' -d /nonexistent -s /sbin/nologin '
+
+useradd -u 26 -g popa3d  -c 'POP3 server'	${VAREMPTY} popa3d
+useradd -u 27 -g sshd  -c 'sshd privsep'	${VAREMPTY} sshd
+useradd -u 28 -g _portmap  -c 'portmap'		${VAREMPTY} _portmap
+useradd -u 29 -g _identd  -c 'identd'		${VAREMPTY} _identd
+useradd -u 30 -g _rstatd  -c 'rpc.rstatd'	${VAREMPTY}  _rstatd
+useradd -u 32 -g _rusersd  -c 'rpc.rusersd'	${VAREMPTY} _rusersd
+useradd -u 33 -g _fingerd  -c 'fingerd'		${VAREMPTY} _fingerd
+
+useradd -u 25 -g smmsp  -c 'Sendmail Message Submission Program'	${NONEXISTENT} smmsp
+useradd -u 71 -g proxy  -c 'Proxy Services' ${NONEXISTENT} proxy
