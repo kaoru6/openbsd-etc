dc0fdfee38b685709d889294b90ec6756ddbc4e4
TAKAHASHI Kaoru <kaoru@kaisei.org>
2002-09-14 10:15:35 +0000

    Update.

--- addgroup.sh
+++ addgroup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-group add -g 25 smmsp
-group add -g 26 popa3d
+
+# from 3.1 to 3.2
+group add -g 66 crontab
+group add -g 72 authpf
+
 group add -g 27 sshd
 group add -g 28 _portmap
 group add -g 29 _identd
@@ -9,6 +12,14 @@ group add -g 32 _rusersd
 group add -g 33 _fingerd
 group add -g 34 _sshagnt
 group add -g 35 _x11
-group add -g 66 crontab
+
+# from 2.9 to 3.0
 group add -g 71 proxy
-group add -g 72 authpf
+group add -g 25 smmsp
+group add -g 26 popa3d
+
+# from 2.8 to 2.9
+group add -g 11 auth
+
+# from 2.3 to 2.4
+#group add -g 70 named
--- adduser.sh
+++ adduser.sh
@@ -2,7 +2,7 @@
 VAREMPTY=' -d /var/empty -s /sbin/nologin '
 NONEXISTENT=' -d /nonexistent -s /sbin/nologin '
 
-useradd -u 26 -g popa3d  -c 'POP3 server'	${VAREMPTY} popa3d
+# from 3.1 to 3.2
 useradd -u 27 -g sshd  -c 'sshd privsep'	${VAREMPTY} sshd
 useradd -u 28 -g _portmap  -c 'portmap'		${VAREMPTY} _portmap
 useradd -u 29 -g _identd  -c 'identd'		${VAREMPTY} _identd
@@ -11,5 +11,10 @@ useradd -u 32 -g _rusersd  -c 'rpc.rusersd'	${VAREMPTY} _rusersd
 useradd -u 33 -g _fingerd  -c 'fingerd'		${VAREMPTY} _fingerd
 useradd -u 35 -g _x11  -c 'X server'		${VAREMPTY} _x11
 
+# from 2.9 to 3.0
 useradd -u 25 -g smmsp  -c 'Sendmail Message Submission Program'	${NONEXISTENT} smmsp
+useradd -u 26 -g popa3d  -c 'POP3 server'	${VAREMPTY} popa3d
 useradd -u 71 -g proxy  -c 'Proxy Services' ${NONEXISTENT} proxy
+
+# from 2.3 to 2.4
+#useradd -u 70 -g named  -c 'BIND Daemon' -d /var/named -s /sbin/nologin named
