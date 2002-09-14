663de5772df4770ceb1e99fdac4d74f19b8cd74e
TAKAHASHI Kaoru <kaoru@kaisei.org>
2002-09-14 09:42:27 +0000

    Update.

--- adduser.sh
+++ adduser.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-VAREMPTY='-d /var/empty -s /sbin/nologin'
-NONEXISTENT='-d /nonexistent -s /sbin/nologin'
+VAREMPTY=' -d /var/empty -s /sbin/nologin '
+NONEXISTENT=' -d /nonexistent -s /sbin/nologin '
 
 useradd -u 26 -g popa3d  -c 'POP3 server'	${VAREMPTY} popa3d
 useradd -u 27 -g sshd  -c 'sshd privsep'	${VAREMPTY} sshd
@@ -9,6 +9,7 @@ useradd -u 29 -g _identd  -c 'identd'		${VAREMPTY} _identd
 useradd -u 30 -g _rstatd  -c 'rpc.rstatd'	${VAREMPTY}  _rstatd
 useradd -u 32 -g _rusersd  -c 'rpc.rusersd'	${VAREMPTY} _rusersd
 useradd -u 33 -g _fingerd  -c 'fingerd'		${VAREMPTY} _fingerd
+useradd -u 35 -g _11  -c 'X server'		${VAREMPTY} _fingerd
 
 useradd -u 25 -g smmsp  -c 'Sendmail Message Submission Program'	${NONEXISTENT} smmsp
 useradd -u 71 -g proxy  -c 'Proxy Services' ${NONEXISTENT} proxy
