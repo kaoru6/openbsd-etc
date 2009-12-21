8d8b5aabd686b32eac5b0a644a524b7b23f05b5d
TAKAHASHI Kaoru <kaoru@kaisei.org>
2009-12-21 05:23:04 +0000

    Add buildrelease.sh.

---
+++ buildrelease.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# check NOPROFILE=no
+cd /usr/src && make obj || exit 1
+cd /usr/src && make build || exit 1
+chmod 000 /usr/sbin/apachectl
+cd /usr/src/distrib/crunch && make obj depend all install || exit 1
+cd /usr/src/etc && env RELEASEDIR=/export/home/kaoru/release DESTDIR=/export/home/kaoru/dest make release || exit 1
